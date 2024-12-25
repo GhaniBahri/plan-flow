76f142bad3f4712f771ee08f59bfba81b589e2f4
abdelghani bahri <bahri.ghanou@gmail.com>
2024-12-25 19:18:58 +0100

    navbar UI

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Navbar from './components/Navbar';
+import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <main className="bg-green-500">
+      <header className="App-header bg-red-400">
+        
+      <Navbar/>
+      </header>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from '../assets/images/PF-full.svg'
+
+function Navbar() {
+  return (
+    <div className="bg-brandiesBlue">
+      <div className="flex flex-row justify-between items-center w-full h-20 px-32 py-8">
+        <a href="/" ><img src={logo} alt="PF home" width={150} height={100} /></a>
+        <button className='border border-white text-white w-28 h-12 rounded-lg text-lg text-center font-semibold'> Login</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
